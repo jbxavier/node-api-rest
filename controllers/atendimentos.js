5a638db7f097cfe271a66aca2e8ce3d486addb97
Jean Bueno Xavier <jbxavier@gmail.com>
2021-12-16 11:29:42 -0300

    Implementado edição de registros - verbo patch

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,10 +1,20 @@
 
+const { lista } = require('../models/atendimentos.js')
 const Atendimento = require('../models/atendimentos.js')
 
 // estamos exportando uma função que recebe o app por parâmetro.
 module.exports = app => {
     // rota GET criada com express
-    app.get('/atendimentos', (req, res) => res.send('você está na rota de atendimentos através do GET'))
+    app.get('/atendimentos', (req, res) => {
+        Atendimento.lista(res)
+    })
+
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id) 
+        
+        Atendimento.buscaPorID(id, res)
+
+    })
 
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body
@@ -14,6 +24,13 @@ module.exports = app => {
         // esse retorno ao cliente tive que remover, causou erro 
         // ERR_HTTP_HEADERS_SENT por enviar mais de uma vez o retorno ao cliente...
         //res.send('Executado POST em atendimento!')
+    })
+
+    app.patch('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        const valores = req.body
+
+        Atendimento.altera(id, valores, res)
     })
 }
 
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -43,6 +43,48 @@ class Atendimento {
             })
         }
     }
+
+    lista(res){
+        const sql = 'SELECT * FROM Atendimentos'
+
+        conexao.query(sql, (erro, resultados) => {
+            if (erro){
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(resultados)
+            }
+        })
+    }
+
+    buscaPorID(id, res){
+        const sql = `SELECT * FROM Atendimentos WHERE id = ${id}`
+
+        conexao.query(sql, (erro, resultados) => {
+            // extraindo somente o objeto do array
+            const atendimento = resultados[0]
+            if (erro){
+                res.status(400).json(erro)
+            } else {
+                // devolvendo somente um objeto do array e não o array resultados
+                res.status(200).json(atendimento) 
+            }
+        })
+    }
+
+    altera(id, valores, res){
+        if (valores.data){
+            valores.data = moment(valores.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss')
+        }
+        const sql = 'UPDATE Atendimentos SET ? WHERE id = ?'
+
+        conexao.query(sql, [valores, id], (erro, resultados) => {
+            if (erro){
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(resultados)
+            }
+        })
+    }
 }
 
 module.exports = new Atendimento
